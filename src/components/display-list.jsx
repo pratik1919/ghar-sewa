de8263a5c189891c338e55042b53ca3c46183d54
Pratik Budhathoki <pratik.budhathoki@gmail.com>
2020-11-03 21:47:00 +0545

    * fetch content from google sheet

--- src/components/display-list.jsx
+++ src/components/display-list.jsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 
+
 class DisplayList extends Component {
     state = {
         contactData: {
-            plumber: [
-                {
-                    name: 'ram',
-                    serviceLocation: 'Chabahil',
-                    contact: '984187653'
-                }
-            ]
+            user: []
         }
     }
+    
+
+    fetchData() {
+        let url = "https://script.googleusercontent.com/macros/echo?user_content_key=1yqlC9jI-LD21SywMbEizDAkg7IXmMKsYXa2EZXaKrY8Z4DN7IJG6R95BaE2VRHmHmnI2fDjqPWtE37xn2S-tEZ8ctAd0EF9m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLagc8KD9TcVt78DlyBgskHNdtrfuBrwcdok0QDZ5zLVzz5lvQGowCfmgfErkRP7vXHCERkbxhUO&lib=Mn88Qfe_vKXmi_jjLH1mckkMTO9OX0aMl";
+        fetch(url)
+            .then(d => d.json())
+            .then(d => {
+                this.setState({contactData: 
+                    {user: d.user}
+                });
+            });
+    }
+
     render() { 
+        this.fetchData();
         return <React.Fragment>
-            <h1>List of contacts</h1>
-            <table className="table table-hover">
+            <h1 id="title">List of contacts <small className="badge badge-secondary">Plumber</small></h1>
+            <table id="worker-table" className="table table-hover table-bordered">
                 <thead>
                     <tr>
                         <th>
@@ -27,19 +36,25 @@ class DisplayList extends Component {
                         <th>
                             Contacts
                         </th>
+                        <th>
+                            Service Type
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        this.state.contactData.plumber.map(plumber =>
-                            <tr>
-                                <td>{plumber.name}</td>
-                                <td>{plumber.serviceLocation}</td>
-                                <td>{plumber.contact}</td>
+                        this.state.contactData.user.map((worker, index) =>
+                            <tr key={index}>
+                                <td>{worker.name}</td>
+                                <td>{worker.serviceLocation}</td>
+                                <td>{worker.phone}</td>
+                                <td>{worker.serviceCategory}</td>
                             </tr>
                     )}
                 </tbody>
-            </table>
+            </table> 
+            
+            
         </React.Fragment>;
     }
 }
